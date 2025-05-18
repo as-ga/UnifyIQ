ce5b5af0cc6a332e588f1dabedd9e48264e77949
Ashutosh Gaurav <mr.gaurav9554@gmail.com>
2025-05-18 13:49:35 +0530

    feat: add notification model with schema and interface for notification management

--- models/index.ts
+++ models/index.ts
@@ -1,3 +1,7 @@
 import UserModel from "./userModel";
+import ProjectModel from "./projectModel";
+import TaskModel from "./taskModel";
+import CommentModel from "./commentModel";
+import NotificationModel from "./notificationModel";
 
-export { UserModel };
+export { UserModel, ProjectModel, TaskModel, CommentModel, NotificationModel };
